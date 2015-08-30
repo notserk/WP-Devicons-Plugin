e266a8dab7f762d8063d4491d23b5e7e694184d8
Kreston  Shirley <notserk@Krestons-MacBook-Pro.local>
2015-08-30 02:52:36 -0400

    Added JSON list to be read into listbox

--- public/editor-button.js
+++ public/editor-button.js
@@ -1,17 +1,37 @@
-(function() {
+(function($) {
+    var items = [];
+
+    $.getJSON('../wp-content/plugins/devicons-tinymce/libs/devicon-master/devicon.json', function(data){
+
+        //console.log(data);
+
+        $.each(data, function(k, v){
+            items.push({text: v.name, value: v.name})
+        });
+
+        //console.dir(items);
+
+    });
+
     tinymce.PluginManager.add('devicons', function( editor, url ) {
         editor.addButton( 'devicons', {
             title: 'Devicons',
             text: 'Devicons',
             onclick: function() {
+                
                 editor.windowManager.open({
                     title: 'Hey There',
-                    body: [{type: 'textbox', name: 'title', label: 'Title'}],
+                    body: [
+                        {type: 'checkbox', name: 'title', label: 'Title', text: 'test'},
+                        {type: 'listbox', name: 'devicons', label: 'Choose Tech', 'values': items}
+
+                    ],
                     onsubmit: function(e){
-                        editor.insertContent(e.data.title);
+                        editor.insertContent(e.data.devicons);
+                        console.log(items);
                     }
                 })
             }
         });
     });
-})();
+})(jQuery);
